1dfc1f9ea0a186dbae500a715104ec9358c76436
jjog22 <john.orozco.giraldo@gmail.com>
2016-12-07 11:10:40 -0500

    changes JJOG

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains functions for pass matrix and prepare variables for
+## correct calculation inverse matrix for first time and cache that 
+## in any other call to solve that matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix gets matrix x and initialize set() get() setinv() getinv()
+## returns list with all variables for solve in the other function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinv <- function(minv) i <<- minv
+  getinv <- function() i
+  list(set = set, get = get,setinv = setinv,getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+
+
+## cacheSolve validate if inverse exists and show it
+## if inverse not exists get matrix from variable and calls solve() function
+## setting in i the inverse matrix with setinv() function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+ ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinv(i)
+  i
 }
